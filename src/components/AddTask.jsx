b15a5d5fdfbb278617f772384eacf6fad70a4662
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-17 14:05:37 -0500

    added a submit task form

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,48 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
+export default function AddTask({ tasks, setTasks }) {
+    const [task, setTask] = useState("")
 
-export default function AddTask() {
+    const newTask = {
+        "done": false, 
+        "task": task
+    }
+
+    const handleNewTask = (e) => {
+        e.preventDefault();
+
+        fetch(`https://todo-c9-api-gp.web.app/tasks`, {
+        method: "POST", 
+        headers: { 
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(newTask),
+        })
+            .then((res) => res.json())
+            .then(setTasks)
+            .then(() => (setTask('')))
+            .catch(err => console.log(err.message))
+    }
+    
     return(
         <>
-
+        <form onSubmit={handleNewTask}>
+            <Form.Group>
+                <Form.Label>Task</Form.Label>
+                <Form.Control 
+                    name="task"
+                    type="task" 
+                    placeholder="Enter Task"
+                    value={task}
+                    className="p-3 hover-effect" 
+                    onChange={e => setTask(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </form>
         </>
     )
 }
